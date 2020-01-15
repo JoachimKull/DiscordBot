0c145499720ddca475658df03efaec15378fb639
JoachimKull <JoachimKull@users.noreply.github.com>
2020-01-15 13:15:54 +0100

    moved the credentials in seperate folder/repo

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@
 */
 
 // load your credentials stored in a seperate file
-var loginCreds = require('./credentials.json');
+var loginCreds = require('../Credentials/credentials.js');
 
 // Import the discord.js module
 const Discord = require('discord.js');
