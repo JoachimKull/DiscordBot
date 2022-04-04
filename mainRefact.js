af32729a78f5bc18d4c9a7c3e992d4342a81f0d7
JoachimKull <37440639+JoachimKull@users.noreply.github.com>
2022-04-04 16:20:47 +0200

    modularized the sounds section

--- mainRefact.js
+++ mainRefact.js
@@ -3,15 +3,22 @@
  * https://gist.github.com/y21/a599ef74c8746341dbcbd32093a69eb8
  */
 
-const getGuildChannelIDfromChannelName = require('./modules/channelID.js');
-const isPrivileged = require('./modules/checkPrivileges.js');
-
 // Load your credentials stored in a separate file
 // var loginCreds = require('../Credentials/credentials.json');
 const loginCreds = require('../Credentials/DF-TestBot_credentials.json');
 
 // Import the discord.js module
 const { Client, Intents } = require('discord.js');
+
+// Import our modules
+const getGuildChannelIDfromChannelName = require('./modules/channelID');
+const isPrivileged = require('./modules/checkPrivileges');
+const listSoundSnippets = require('./modules/sounds');
+const playSnippet = require('./modules/sounds');
+
+// Defines
+const soundsFolder = './SoundSnippets/';
+const fs = require('fs');
 
 // Get all intents and create an instance of a Discord client
 const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] }, { ws: { intents: Intents.ALL } });
@@ -25,7 +32,7 @@ client.on('ready', () => {
     console.log('LegendBot is ready! - ' + date.toLocaleString());
 
     // Set a game activity for the bot
-    client.user.setActivity("LegendBot | !help");
+    client.user.setActivity("LegendBot | ?help");
 });
 
 // Create an event listener for messages
@@ -36,7 +43,7 @@ client.on('message', userMessage => {
     var channelBotCommands = getGuildChannelIDfromChannelName(userMessage, '🤖bot-commands');
 
     // If the origin of the message is the bot-commands channel or the message author is not our bot or the message author is an admin - do nothing
-    if (lowercasemsg.startsWith('!')) {
+    if (lowercasemsg.startsWith('!') || lowercasemsg.startsWith('?') || lowercasemsg.startsWith('/')) {
         if (!isPrivileged(userMessage, channelBotCommands)) {
             return;
         }
@@ -49,9 +56,19 @@ client.on('message', userMessage => {
         console.log('Ping from: ' + userMessage.member.user.username);
     }
 
-    if (lowercasemsg === '!cmds' || lowercasemsg === '!help') {
+    if (lowercasemsg === '?cmds' || lowercasemsg === '?help') {
         // Print all existing commands
-        userMessage.reply('Hey you! \nI only understand certain commands. Here is a list of them: \n-> "**!roles**" - shows a list of all available roles \n-> "**!sounds**" - shows a list of all sound snippets \nGo ahead and try it yourself under the channel <#' + channelBotCommands + '>').catch((e) => { console.log(e); });
+        userMessage.reply('Hey you! \nI only understand certain commands. Here is a list of them: \n-> "**?sounds**" - shows a list of all sound snippets \nGo ahead and try it yourself under the channel <#' + channelBotCommands + '>').catch((e) => { console.log(e); });
+    }
+
+    // Display all available sound snippets
+    if (lowercasemsg === '?sounds') {
+        listSoundSnippets(userMessage, fs, soundsFolder);
+    }
+
+    // Play the sound snippet if it has found one
+    if (userMessage.content.startsWith('/')) {
+        playSnippet(lowercasemsg, fs, soundsFolder)
     }
 });
 
---
+++ modules/sounds.js
@@ -0,0 +1,60 @@
+/**
+ * Prints a list of all available sound snippets
+ * @param {*} message is used to get all the needed information e.g. channel id in which it was sent in
+ * @param {*} fs represents the filesystem
+ * @param {*} soundsFolder path to the folder with the snippets
+ */
+function listSoundSnippets(message, fs, soundsFolder) {
+    message.reply('Try playing a snippet by typing "**/**_filename_" \nHere are the available sounds:\n').catch((e) => { console.log(e); });
+    var sounds = [];
+    // Iterate over snippets
+    fs.readdir(soundsFolder, (err, files) => {
+        files.forEach(file => {
+            sounds.push('- ' + file.split(".", 1));
+        });
+        message.channel.send(sounds).catch((e) => { console.log(e); });
+        message.channel.send('_May I suggest you to try_ **/click**').catch((e) => { console.log(e); });
+    });
+}
+module.exports = listSoundSnippets;
+
+/**
+ * Plays the selected snippet in a voice channel
+ * @param {*} message is used to get all the needed information e.g. channel id in which it was sent in
+ * @param {*} fs represents the filesystem
+ * @param {*} soundsFolder path to the folder with the snippets
+ * @returns a message if the user is not in a voice channel
+ */
+function playSnippet(message, fs, soundsFolder) {
+    // Voice only works in guilds
+    if (!message.guild) return;
+    try {
+        // Remove the / from the message and add the .mp3 ending
+        var concatMsg = lowerCaseMessage.concat('.mp3').slice(1);
+        var pathToFile = soundsFolder.concat(concatMsg);
+        var vc = message.member.voice.channel;
+
+        // Iterate over snippets
+        fs.readdir(soundsFolder, (err, files) => {
+            files.forEach(file => {
+                if (file === concatMsg) {
+                    // Only try to join the sender's voice channel if they are in one themselves
+                    if (vc) {
+                        vc.join().then(connection => {
+                            const dispatcher = connection.play(pathToFile, { volume: 0.5, });
+                            dispatcher.on('finish', () => {
+                                vc.leave();
+                                // console.log('Finished playing: ' + concatMsg);
+                            });
+                        });
+                    } else {
+                        message.reply('You need to join a voice channel first!');
+                    }
+                }
+            });
+        });
+    } catch (error) {
+        console.log('Unknown input for sound snippet: ' + message.content);
+    }
+}
+module.exports = playSnippet;
